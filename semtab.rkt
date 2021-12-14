36ad175b85858bf67a3eea528f9ebf6a113f1d85
Opeka-2201 <65912187+Opeka-2201@users.noreply.github.com>
2021-12-14 02:14:30 +0100

    semtab OK + satis + tauto + contra

--- semtab.rkt
+++ semtab.rkt
@@ -3,56 +3,100 @@
 ;; FIRRINCIELI Maxime - s190792
 ;; LOUIS Arthur       - s191230
 
-(define isDevelopped? (lambda (ls)
-                            (if (null? ls) #t
-                     (let* ((head (car ls))
-                            (rest (cdr ls)))
-                   (cond
-                     ((or (not (list? head)) (and (eq? (car head) 'NOT) (not (list? (cadar ls))))) (isDevelopped? rest))
-                     (else #f))))))
+#lang racket
+
+(define (atom? x)
+        (and (not (null? x))
+              (not (pair? x))))
+
+(define (isDevelopped? ls)
+        (if (null? ls) 
+          #t
+          (let* ((head (car ls)) (rest (cdr ls)))
+            (cond
+              ((or (not (list? head)) (and (eq? (car head) 'NOT) (not (list? (cadar ls))))) (isDevelopped? rest))
+              (else #f)))))
+
+(define (semtab ls)
+        (if (null? ls) ls
+            (let* ((head (car ls)) (rest (cdr ls)))
+                  (cond
+                    ((isDevelopped? ls) ls)
+                    ;_______________________________
+                    ;Cas de base
+                    ((or (not (list? head)) (and (eq? (car head) 'NOT) (not (list? (cadar ls))))) (semtab (append rest (list head))))
+                    ;_______________________________
+                    ;NOT NOT
+                    ((and (eq? (car head) 'NOT) (list? (cdr head)) (eq? (caadr head) 'NOT) (append rest (semtab (cdadr head)))))
+                    ;_______________________________
+                    ;AND
+                    ((eq? (car head) 'AND) (semtab (append (list (cadr head) (caddr head)) rest)))
+                    ;NOT AND
+                    ((and (eq? (car head) 'NOT) (eq? (caadr head) 'AND)) (semtab (cons (list 'OR (list 'NOT (cadadr head)) (list 'NOT (car (cddadr head)))) rest)))
+                    ;_______________________________
+                    ;OR
+                    ((eq? (car head) 'OR) (list (semtab (cons (cadr head) rest)) (semtab (cons (caddr head) rest))))
+                    ;NOT OR
+                    ((and (eq? (car head) 'NOT) (eq? (caadr head) 'OR)) (semtab (cons (list 'AND (cons 'NOT (cadadr head)) (list 'NOT (car (cddadr head)))) rest)))
+                    ;_______________________________
+                    ;IFTHEN
+                    ((eq? (car head) 'IFTHEN) (semtab (cons (list 'OR (list 'NOT  (cadr head)) (caddr head)) rest)))
+                    ;NOT IFTHEN
+                    ((and (eq? (car head) 'NOT) (eq? (caadr head) 'IFTHEN)) (semtab (cons (list 'AND (cadadr head) (list 'NOT (car (cddadr head)))) rest)))
+                    ;_______________________________
+                    (else (error "Error : unhandeld case"))))))
+
+(define (not-list ls)
+        (if (null? ls)
+          ls
+          (let* ((head (car ls)) (rest (cdr ls)))
+            (cond
+              ((atom? head) (cons (append (list 'NOT) (list head)) (not-list rest)))
+              (else (cons (append (list 'NOT)  (list head)) (not-list rest)))))))
+
+(define (contr e ls)
+        (if (null? ls) 
+          #f
+          (let* ((head (car ls)) (rest (cdr ls)))
+            (cond
+              ((atom? head) (contr e rest))
+              ((eq? e (cadr head)) #t)
+              (else (contr e rest))))))
+
+(define (contradiction-in-branch? ls)
+        (if (null? ls)
+          #f
+          (let* ((head (car ls)) (rest (cdr ls)))
+            (cond
+              ((atom? head) (or (contr head (cdr ls)) (contradiction-in-branch? rest)))
+              (else (or (contr (cadr head) (semtab (not-list (cdr ls)))) (contradiction-in-branch? rest)))))))
 
 (define (satisfiable? ls)
-  )
+        (let ((smt (semtab ls)))
+                (letrec ((loop (lambda (h)
+                                (cond
+                                  ((null? h) #f)
+                                  ((isDevelopped? h) (not (contradiction-in-branch? h)))
+                                  (else (or (loop (car h)) (loop (cdr h))))))))
+                  (loop smt))))
 
-(define semtab (lambda (ls)
-                 (if (null? ls) ls
-                     (let* ((head (car ls))
-                            (rest (cdr ls)))
-                   (cond
-                     ((isDevelopped? ls) ls)
-                     ;_______________________________
-                     ;Cas de base
-                     ((or (not (list? head)) (and (eq? (car head) 'NOT) (not (list? (cadar ls))))) (semtab (append rest (list head))))
-                     ;_______________________________
-                     ;NOT NOT
-                     ((and (eq? (car head) 'NOT) (list? (cdr head)) (eq? (caadr head) 'NOT) (semtab (cdadr head))))
-                     ;_______________________________
-                     ;AND
-                     ((eq? (car head) 'AND) (semtab (append (list (cadr head) (caddr head)) rest)))
-                     ;NOT AND
-                     ((and (eq? (car head) 'NOT) (eq? (caadr head) 'AND)) (semtab (cons (list 'OR (list 'NOT (cadadr head)) (list 'NOT (car (cddadr head)))) rest)))
-                     ;_______________________________
-                     ;OR
-                     ((eq? (car head) 'OR) (list (semtab (cons (cadr head) rest)) (semtab (cons (caddr head) rest))))
-                     ;NOT OR
-                     ((and (eq? (car head) 'NOT) (eq? (caadr head) 'OR)) (semtab (cons (list 'AND (cons 'NOT (cadadr head)) (list 'NOT (car (cddadr head)))) rest)))
-                     ;_______________________________
-                     ;IFTHEN
-                     ((eq? (car head) 'IFTHEN) (semtab (cons (list 'OR (list 'NOT  (cadr head)) (caddr head)) rest)))
-                     ;NOT IFTHEN
-                     ((and (eq? (car head) 'NOT) (eq? (caadr head) 'IFTHEN)) (semtab (cons (list 'AND (cadadr head) (list 'NOT (car (cddadr head)))) rest)))
-                     ;_______________________________
-                     (else (display "Erreur: cas non traité.")))))))
+(define (tautology? ls)
+        (if (null? ls) 
+          #t
+          (not (satisfiable? (not-list ls)))))
+
+(define (contradiction? ls)
+        (if (null? ls)
+          #f
+          (not (satisfiable? ls))))
+
+(define (valid? ls) ls)
+
+(define (models ls) ls)
+
+(define (counterexamples ls) ls)
 
 ;(require racket/trace)
-;(trace semtab)
+;(trace contradiction-in-branch?)
 
-(define foo '((IFTHEN p (IFTHEN q r)) (NOT (IFTHEN (IFTHEN p q) r))))
-(define foot '((IFTHEN p (IFTHEN q r))))
-(define test '((OR (OR p q) (NOT s))))
-(define test1 '((NOT (NOT (NOT (NOT (NOT (NOT p))))))))
-(define test2 '(p (AND q (OR p q)) (IFTHEN r (AND s q))))
-(define test3 '((OR p q) b c d e))
-(define test4 '(p (NOT (AND q p))))
-(define test5 '(r s (OR p q)))
-(define rep (semtab foo))
+(contradiction? '(AND a (NOT a)))
